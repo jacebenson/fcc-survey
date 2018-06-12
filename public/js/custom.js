a2f2bdd2112739e7bbf7350d4260316e99ea6e05
Jace Benson <jace.benson@gmail.com>
2018-06-12 14:27:26 -0500

    added source/referer to submission

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,41 +1,43 @@
+/* global $ */
 $(document).ready(function () {
-    $("#ajax-submit").click(function () {
-        console.log("ajax started...");
-        if (!$('form')[0].checkValidity()) {
-            // If the form is invalid, submit it. The form won't actually submit;
-            // this will just cause the browser to display the native HTML5 error messages.
-            $('#submit').click();
-            return false;
-        }
-        var title = $("#comments").val().substring(0, 30);
-        var body = "";
-        body += "Name: " + $("#name").val() + "<br/>";
-        body += "Email: " + $("#email").val() + "<br/>";
-        body += "Contact them: " + $("#dropdown").val() + "<br/>";
-        body += "Comments: " + $("#comments").val().replace(/\n/gm,"<br/>")
-        var data = {
-            endpoint: "jacebenson/blog",
-            title: title,
-            body: body,
-            labels: $('input[name=radios]:checked', '#survey-form').val()
-        };
-        console.log(data);
-        var settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": "https://gitlab-issue.glitch.me/api/issue",
-            "method": "POST",
-            "headers": {
-              "content-type": "application/json"
-            },
-            "processData": false,
-            "data": JSON.stringify(data)
-          }
-          
-          $.ajax(settings).done(function (response) {
-            console.log(response);
-            $('fieldset').prop('disabled', true);
-            $('#myModal').modal('show');
-          });
+  $('#ajax-submit').click(function () {
+    $('fieldset').prop('disabled', true);
+    if (!$('form')[0].checkValidity()) {
+      // If the form is invalid, submit it. The form won't actually submit;
+      // this will just cause the browser to display the native HTML5 error messages.
+      $('#submit').click();
+      return false;
+    }
+    var parsedUrl = new URL(window.location.href);
+    var title = $('#comments').val().substring(0, 30);
+    var body = '';
+    body += 'Name: ' + $('#name').val() + '<br/>';
+    body += 'Email: ' + $('#email').val() + '<br/>';
+    body += 'Referrer: ' + parsedUrl.searchParams.get('source') + '<br/>';
+    body += 'Contact them: ' + $('#dropdown').val() + '<br/>';
+    body += 'Comments: ' + $('#comments').val().replace(/\n/gm, '<br/>');
+    var data = {
+      endpoint: 'jacebenson/blog',
+      title: title,
+      body: body,
+      labels: $('input[name=radios]:checked', '#survey-form').val()
+    };
+    console.log(data);
+    var settings = {
+      'async': true,
+      'crossDomain': true,
+      'url': 'https://gitlab-issue.glitch.me/api/issue',
+      'method': 'POST',
+      'headers': {
+        'content-type': 'application/json'
+      },
+      'processData': false,
+      'data': JSON.stringify(data)
+    };
+
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+      $('#myModal').modal('show');
     });
+  });
 });
